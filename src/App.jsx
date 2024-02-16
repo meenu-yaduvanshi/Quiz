744ce651b6b6da146f16d8f3660c38820a027c52
Meenu Yaduvanshi <meenuyaduvanshi24@gmail.com>
2024-02-16 22:04:04 +0530

    make quiz app

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+
+import { useState } from 'react'
+import Data from './component/quize.json'
+import Questions from './component/Question'
+import Options from './component/Option'
+import "./style.css";
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [selectedIndex, setSelectedIndex] = useState(null)
+  const [correctAns, setCorrectAns] = useState(null)
+
+  const handleClick = (i) => {
+    setSelectedIndex(null)
+    setCorrectAns(null)
+    setCount((previousState) => {
+      return previousState + 1
+    }
+    )
+    if (count === 6) {
+      setCount(0)
+    }
+  }
+
+  function optionClick(i) {
+    setSelectedIndex(i)
+    setCorrectAns(Data[count].Ans)
+  }
+
+  return (
+    <div className='container'>
+      <Questions que={Data[count].questions} />
+      {Data[count].options.map((option, i) => {
+        return (<Options key={i} option={option} answer={Data[count].options} i={i}
+          optionClick={optionClick} correctAns={correctAns} selectedIndex={selectedIndex} />)
+      })}
+      <button className='btn' onClick={handleClick}>Next</button>
+    </div>
+  )
+}
+
+export default App
+
